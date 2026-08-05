f59c69ed48d2146d70033dbcbf8de92de7ca13c1
Salih Derin <derin.salih@gmx.de>
2026-08-05 18:24:14 +0200

    add formGroup progress for formular

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
-export class Contact {}
+export class Contact {
+
+  contactForm = new FormGroup({
+    
+  })
+}
