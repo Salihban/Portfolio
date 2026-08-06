586364c2c97b6dac5f01b49e79e69c8e111a3ed4
Salih Derin <derin.salih@gmx.de>
2026-08-06 15:39:52 +0200

    fix form group in contact

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
-import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators, FormControlName } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -11,6 +11,17 @@ import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angula
 export class Contact {
 
   contactForm = new FormGroup({
-    
+    name: new FormControl ('', {
+      validators: [Validators.required, Validators.minLength(3)]
+    }),
+    email: new FormControl ('', {
+      validators: [Validators.required, Validators.email]
+    }),
+    message: new FormControl ('', {
+      validators: [Validators.required, Validators.minLength(10)]
+    }),
+    privacy: new FormControl(false, {
+      validators: [Validators.requiredTrue]
+    })
   })
 }
