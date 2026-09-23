a2df404205b2fa3c52aa1cd91e69962c2f4da1e6
Salih Derin <derin.salih@gmx.de>
2026-09-23 11:16:53 +0200

    feat: create send email message

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,27 +1,57 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
-import { ReactiveFormsModule, FormGroup, FormControl, Validators, FormControlName } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
+  standalone: true,
   imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
 export class Contact {
+  private http = inject(HttpClient);
+  isSubmitted = false;
+
+  
 
   contactForm = new FormGroup({
-    name: new FormControl ('', {
-      validators: [Validators.required, Validators.minLength(3)]
-    }),
-    email: new FormControl ('', {
-      validators: [Validators.required, Validators.email]
-    }),
-    message: new FormControl ('', {
-      validators: [Validators.required, Validators.minLength(10)]
-    }),
-    privacy: new FormControl(false, {
-      validators: [Validators.requiredTrue]
-    })
-  })
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('', Validators.required),
+    privacy: new FormControl(false, Validators.requiredTrue)
+  });
+
+  onSubmit() {
+    if (this.contactForm.invalid) {
+      this.contactForm.markAllAsTouched();
+      return;
+    }
+
+    const formData = {
+      access_key: '1387b5a4-f7e8-495d-a3b6-78c84eb61644', 
+      name: this.contactForm.value.name,
+      email: this.contactForm.value.email,
+      message: this.contactForm.value.message
+    };
+
+    this.http.post<any>('https://api.web3forms.com/submit', formData).subscribe({
+      next: (response) => {
+        console.log('Successfully sent!', response);
+
+        if (!response.success) {
+          alert(response.message || response.body?.message);
+          return;
+        }
+        this.isSubmitted = true;
+        this.contactForm.reset();
+
+        setTimeout(() => this.isSubmitted = false, 3000);
+      },
+      error: (error) => {
+        console.error('Error sending:', error);
+      }
+    });
+  }
 }
